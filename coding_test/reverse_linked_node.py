274bce5fdcac08d5a3b24785e2e244821cf3139f
hoetaek <hoetaek@student.snue.ac.kr>
2022-08-13 10:28:52 +0900

    recursive version

--- coding_test/reverse_linked_node.py
+++ coding_test/reverse_linked_node.py
@@ -14,3 +14,17 @@ class Solution:
             curr = next_node
         
         return prev    
+
+
+class Solution2:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return newHead
