3e041922413c2d4f757ee1aa6891e817b4154747
hoetaek <hoetaek@student.snue.ac.kr>
2022-08-21 16:30:24 +0900

    same tree second solution

--- coding_test/same_tree.py
+++ coding_test/same_tree.py
@@ -18,3 +18,14 @@ class Solution:
             return res
         
         return dfs(p, []) == dfs(q, [])
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == None or q == None:
+            if p or q:
+                return False
+            return True
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
